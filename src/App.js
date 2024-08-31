faf1cd0df755cd535f91a5551bc65278a7f9b6ba
Hyejung Na <hyejung.na01@gmail.com>
2024-09-01 05:01:55 +1000

    Style navigation bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import AppLayout from './layout/AppLayout';
 import { Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage';
--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,7 +1,63 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Outlet } from 'react-router-dom';
+import netflixLogo from '../assets/netflix-logo.png';
+import './AppLayout.style.css';
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const AppLayout = () => {
-  return <div>navbar</div>;
+  return (
+    <div>
+      <Navbar
+        expand='lg'
+        className='bg-body-tertiary'
+        bg='dark'
+        data-bs-theme='dark'
+      >
+        <Container fluid>
+          <Navbar.Brand href='#'>
+            <img
+              src={netflixLogo}
+              alt='netflix logo'
+              className='netflix-logo'
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='navbarScroll' />
+          <Navbar.Collapse id='navbarScroll'>
+            <Nav
+              className='me-auto my-2 my-lg-0'
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link as={Link} to='/' style={{ color: 'white' }}>
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to='/movies' style={{ color: 'white' }}>
+                Movies
+              </Nav.Link>
+            </Nav>
+            <Form className='d-flex'>
+              <Form.Control
+                type='search'
+                placeholder='Search'
+                className='me-2'
+                aria-label='Search'
+              />
+              <Button variant='outline-success' className='search-btn'>
+                <FaSearch className='search-icon' />
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
+  );
 };
 
 export default AppLayout;
