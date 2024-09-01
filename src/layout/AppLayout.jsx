2067865dbb97b541db9ff9fc9e8b54085d672482
Hyejung Na <hyejung.na01@gmail.com>
2024-09-02 00:49:22 +1000

    Implement API logics and banner component

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,55 +1,55 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Outlet } from 'react-router-dom';
-import netflixLogo from '../assets/netflix-logo.png';
-import './AppLayout.style.css';
-import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Outlet } from "react-router-dom";
+import netflixLogo from "../assets/netflix-logo.png";
+import "./AppLayout.style.css";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div>
       <Navbar
-        expand='lg'
-        className='bg-body-tertiary'
-        bg='dark'
-        data-bs-theme='dark'
+        expand="lg"
+        className="bg-body-tertiary"
+        bg="dark"
+        data-bs-theme="dark"
       >
         <Container fluid>
-          <Navbar.Brand href='#'>
+          <Navbar.Brand href="#">
             <img
               src={netflixLogo}
-              alt='netflix logo'
-              className='netflix-logo'
+              alt="netflix logo"
+              className="netflix-logo"
             />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls='navbarScroll' />
-          <Navbar.Collapse id='navbarScroll'>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
             <Nav
-              className='me-auto my-2 my-lg-0'
-              style={{ maxHeight: '100px' }}
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Nav.Link as={Link} to='/' style={{ color: 'white' }}>
+              <Nav.Link as={Link} to="/" style={{ color: "white" }}>
                 Home
               </Nav.Link>
-              <Nav.Link as={Link} to='/movies' style={{ color: 'white' }}>
+              <Nav.Link as={Link} to="/movies" style={{ color: "white" }}>
                 Movies
               </Nav.Link>
             </Nav>
-            <Form className='d-flex'>
+            <Form className="d-flex">
               <Form.Control
-                type='search'
-                placeholder='Search'
-                className='me-2'
-                aria-label='Search'
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
               />
-              <Button variant='outline-success' className='search-btn'>
-                <FaSearch className='search-icon' />
+              <Button variant="outline-success" className="search-btn">
+                <FaSearch className="search-icon" />
               </Button>
             </Form>
           </Navbar.Collapse>
