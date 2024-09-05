dcb2af1dcc648bcee05722af7ac55afe1e215411
Hyejung Na <hyejung.na01@gmail.com>
2024-09-06 00:26:24 +1000

    update route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AppLayout from './layout/AppLayout';
-import { Routes, Route } from 'react-router-dom';
-import Homepage from './pages/Homepage/Homepage';
-import MoviePage from './pages/Movies/MoviePage';
-import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
-import NotFoundPage from './pages/NotFoundpage/NotFoundPage';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AppLayout from "./layout/AppLayout";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage/Homepage";
+import MoviePage from "./pages/Movies/MoviePage";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
+import NotFoundPage from "./pages/NotFoundpage/NotFoundPage";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<AppLayout />}>
+      <Route path="/" element={<AppLayout />}>
         <Route index element={<Homepage />} />
-        <Route path='movies'>
+        <Route path="movies">
           <Route index element={<MoviePage />} />
-          <Route path=':id' element={<MovieDetailPage />} />
+          <Route path=":id" element={<MovieDetailPage />} />
         </Route>
-        {/* <Route path='/movies' element={<MoviePage />} />
-        <Route path='/movies/:id' element={<MovieDetailPage />} /> */}
+        {/* <Route path="/movies" element={<MoviePage />} /> */}
+        {/* <Route path="/movies/:id" element={<MovieDetailPage />} /> */}
       </Route>
 
-      <Route path='*' element={<NotFoundPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
