d8027bf48f1c16e785e541a9f16168fde60cde5c
Hyejung Na <hyejung.na01@gmail.com>
2025-08-20 03:37:30 +1000

    feat: add Loader component

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
 import Alert from "react-bootstrap/Alert";
 import "./Banner.style.css";
+import Loader from "../Loader/Loader";
 
 const Banner = () => {
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
   console.log("popularMovies", data);
   if (isLoading) {
-    return <h1>Loading...</h1>;
+    return <Loader />;
   }
   if (isError) {
     return <Alert variant="danger">{error.message}</Alert>;
